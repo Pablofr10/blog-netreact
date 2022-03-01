17214f17759ed443aee68e287f2003217605a3bf
Pablorf10 <rafanet.live@gmail.com>
2022-02-28 22:59:35 -0300

    Criando context

---
+++ backend/backend/Context/BlogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Context
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostCategoria> PostCategoria { get; set; }
+    }
+}
---
+++ backend/backend/Models/PostCategoria.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Models
+{
+    public class PostCategoria
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public Post Posts { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
